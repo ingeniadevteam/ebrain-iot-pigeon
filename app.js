ae1196866e9518ddfc9e79918648cb5691a42f64
Víctor Navarro <vicnala@gmail.com>
2022-12-05 10:11:36 +0100

    Accurated loop timing

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 'use strict';
 
+const round = require('round-to');
 const App = require('.');
 const pids = require('./pids');
+const acUnits = require('./ac_units');
+
+const nextRun = async (app, t) => {
+    let next = 0;
+    const mils = t[0] * 1000 + t[1] / 1000000;
+    if (mils < app.attributes.SMAPLE_RATE) next = app.attributes.SMAPLE_RATE - mils;
+    app.logger.debug(`mils ${round(mils, 1)} next ${round(next, 1)} rate ${round(mils + next, 1)}`);
+    await new Promise(resolve => setTimeout(() => resolve(), next));
+}
 
 async function main () {
     // create a new App
     const app = await App();
-
-    app.logger.info(`ATTRIBUTES ${JSON.stringify(app.attributes)}`);
-
     // init the PIDs
     pids.init(app);
+    // init the AC Units
+    acUnits.init(app);
 
     while (true) {
+        // get the time for rate pourposes
+        const t = process.hrtime();
+
         // run the PIDs
         pids.run(app);
+        // run the AC Units
+        acUnits.run(app);
 
 
-        await new Promise(resolve => setTimeout(() => resolve(), app.attributes.SMAPLE_RATE));
+        // setup next run
+        await nextRun(app, process.hrtime(t));
     }
 };
 
